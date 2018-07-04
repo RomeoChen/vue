e71b9a7ebd5a08b8840748df5a8712f21a729fbb
RomeoChen <18186483358@163.com>
2018-07-04 14:06:38 +0800

    add slider滑块

---
+++ src/components/slider.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="block">
+    <span class="demonstration">默认</span>
+    <el-slider v-model="value"></el-slider>
+  </div>
+</template>
+    
+<script>
+  export default {
+    data() {
+      return {
+        value: 0
+      }
+    }
+  }
+</script>
+
+<style>
+  .block {
+    padding: 30px 24px;
+    border: 1px solid #eff2f6;
+  }
+  .demonstration {
+    line-height: 40px;
+  }
+  .el-slider {
+    width: 70%;
+    float: right;
+    margin-right: 20px; 
+  }
+</style>
