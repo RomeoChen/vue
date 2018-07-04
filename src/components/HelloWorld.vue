95c8b46b847f2dc935e0dbcbbf6b08fa8d141df7
RomeoChen <18186483358@163.com>
2018-07-04 15:07:22 +0800

    添加标签页、面包屑、上传三个组件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="hello">
+    <div class="tabs">
+      <tabs/>
+    </div>
+    <div class="breadcrumb">
+      <breadcrumb/>
+    </div>
     <div class="cascader">
       <cascader></cascader>
     </div>
     <div class="slider">
       <slider/>
+    </div>
+    <div class="upload">
+      <upload/>
     </div>
   </div>
 </template>
@@ -12,17 +21,30 @@
 <script>
 import cascader from '@/components/cascader'; 
 import slider from '@/components/slider'
+import upload from '@/components/upload'
+import tabs from '@/components/tabs'
+import breadcrumb from '@/components/breadcrumb'
 export default {
   name: 'HelloWorld',
   components: {
     cascader, 
-    slider
+    slider,
+    upload,
+    tabs,
+    breadcrumb,
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.upload {
+  border: 1px solid #eff2f6;
+}
 
+.breadcrumb {
+  padding: 20px;
+  border: 1px solid #eff2f6;
+}
 
 </style>
